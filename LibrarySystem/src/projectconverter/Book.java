56f5919031adbc4871ae1798856df5d967689676
Chanaka Roshane <chanakaroshane.123@gmail.com>
2018-01-07 13:05:07 +0530

    final working

--- LibrarySystem/src/projectconverter/Book.java
+++ LibrarySystem/src/projectconverter/Book.java
@@ -6,24 +6,51 @@
 package projectconverter;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Chath
  */
-public class Book {
+public class Book implements Manage{
     
+    private String bkname,description,authName,genre;
+    private int availability;
     
-    public String addBook(String name,String description,String authName, String genre, int availability) throws ClassNotFoundException, SQLException
+    public void setAvailability(int availability) {
+        this.availability = availability;
+    }
+
+    public void setName(String bkname) {
+        this.bkname = bkname;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setAuthName(String authName) {
+        this.authName = authName;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    
+
+    public String addBook() throws ClassNotFoundException, SQLException
     {
     String msg="-1";
     String sql = "INSERT INTO book(name,description,authName,genre,availability) "
-            + "VALUES('"+name+"','"+description+"','"+authName+"','"+genre+"'," +availability+")";
+            + "VALUES('"+bkname+"','"+description+"','"+authName+"','"+genre+"'," +availability+")";
     try{
     DatabaseCon con = new DatabaseCon();
     msg=con.insert(sql);
+    msg= "Success!";
     }
     
     catch(SQLException e){
@@ -36,5 +63,49 @@ public class Book {
     finally{
     return msg;
     }
+    }
+    
+    public static ResultSet getBooks() throws ClassNotFoundException, SQLException    {
+    String sql = "SELECT name FROM book";
+    DatabaseCon con = new DatabaseCon();
+    return con.select(sql);
+    }
+
+    public static ResultSet getBookDetails(String name) throws ClassNotFoundException, SQLException {
+        //
+        //selecting book from list will send name of book which is then used in where clause of sql
+        //overload select methodin DatabaseCon
+    String sql = "SELECT name,description,authName,genre,availability FROM book WHERE name=?";
+    DatabaseCon con = new DatabaseCon();
+    return con.select(sql,name);
+    }
+ 
+
+    @Override
+    public ResultSet search(String name) {
+     String sql = "SELECT name,description,authName,genre,availability FROM book WHERE name LIKE '%"+name+"%'";
+    ResultSet rs = null;
+     DatabaseCon con = new DatabaseCon();
+        try {
+            rs = con.select(sql);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(Book.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally{
+        return rs; }
+    }
+
+    @Override
+    public ResultSet sort() {
+        String sql = "SELECT name FROM book SORT BY ASC";
+        ResultSet rs = null;
+        DatabaseCon con = new DatabaseCon();
+        try {
+           rs = con.select(sql);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(Book.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally{
+        return rs; }
     }
 }
